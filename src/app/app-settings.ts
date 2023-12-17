b05115fdb15822739c36301003a9d5e0018ae3a9
Ryuytyuu-Greseven <rukku93223@gmail.com>
2023-12-17 23:37:32 +0530

    added crypto encryption

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, map, tap } from 'rxjs';
+import * as CryptoJS from 'crypto-js';
 import { environment } from 'src/environments/environement';
 
 @Injectable({
@@ -25,15 +26,46 @@ export class AppSettings {
   };
 
   requestServer(body: any, url: string) {
-    return this.httpClient.post(url, body).pipe(
-      tap((response: any) => {
+    const encData = { stinky: this.encrypt(body) };
+
+    return this.httpClient.post(url, encData).pipe(
+      map((response: any) => {
         console.log('response', response);
         if (response.statusCode === 401) {
           sessionStorage.clear();
           this.router.navigate(['/login']);
         }
+        response = this.decrypt(response.spicy);
         return response;
       })
     );
+  }
+
+  encrypt(dataToEncrypt: any) {
+    try {
+      const encryptedText = CryptoJS.AES.encrypt(
+        JSON.stringify(dataToEncrypt),
+        environment.enc_key
+      ).toString();
+      // console.log('\n\nEncoded String', encryptedText);
+      // console.log(this.decryptService(encryptedText));
+
+      return encryptedText;
+    } catch (error) {
+      return;
+    }
+  }
+
+  // decrypt data using nodejs crypto module
+  decrypt(dataToDecrypt: any) {
+    try {
+      const bytes = CryptoJS.AES.decrypt(dataToDecrypt, environment.enc_key);
+      const decryptedText = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+      // console.log('\n\nDecoded String', decryptedText);
+
+      return decryptedText;
+    } catch (error) {
+      return false;
+    }
   }
 }
